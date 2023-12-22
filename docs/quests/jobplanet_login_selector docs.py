4aaa7729674ee4a2cc2767d885949b7b465cdcef
youjin0512 <kelly2.youjin.kim@gmail.com>
2023-12-22 17:56:50 +0900

    add something

---
+++ docs/quests/jobplanet_login_selector docs.py
@@ -0,0 +1,9 @@
+# 1) 잡플래닛 접속
+# url : https://www.jobplanet.co.kr/job
+
+# 2) 로그아웃 된 상태에서 로그인 클릭
+# url : https://www.jobplanet.co.kr/users/sign_in?_nav=gb
+
+# id : #user_email
+# password : #user_password
+# sign in : div > section.section_email.er_r > fieldset > button
